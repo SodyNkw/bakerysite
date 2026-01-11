0483a38973a1e8f5b49776a78cf95c4a9b5fdb53
Sody Nkwonta <nkwonta42@gmail.com>
2026-01-11 14:57:25 -0400

    Added menu item model and feature to send email from cart form

--- src/main/java/com/example/bakerysite/bakerysite/controller/HomeController.java
+++ src/main/java/com/example/bakerysite/bakerysite/controller/HomeController.java
@@ -3,27 +3,28 @@ package com.example.bakerysite.bakerysite.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.bakerysite.bakerysite.model.Customer;
 import com.example.bakerysite.bakerysite.repository.CustomerRepository;
 import com.example.bakerysite.bakerysite.service.CustomerService;
+import com.example.bakerysite.bakerysite.service.MailSenderService;
+
+import org.apache.commons.lang3.RandomStringUtils;
 
 @Controller
 public class HomeController {
 
     private final CustomerService customerService;
+    private final MailSenderService mailSenderService;
 
-    private final CustomerRepository customerRepository;
+    //private final CustomerRepository customerRepository;
 
-    public HomeController(CustomerRepository customerRepository, CustomerService customerService) {
-        this.customerRepository = customerRepository;
+    public HomeController(CustomerService customerService, MailSenderService mailSenderService) {
         this.customerService = customerService;
+        this.mailSenderService = mailSenderService;
     }
 
     // public HomeController(CustomerService customerService) {
@@ -58,13 +59,22 @@ public class HomeController {
 
     @PostMapping("/create")
     public String saveCustomer(@ModelAttribute Customer customer) {
-        //System.out.println("Saving customer: " + customer); // DEBUG
         customerService.addCustomer(customer);
-        // Customer saved = customerRepository.save(customer);
-        // System.out.println("Saved customer id: " + saved.getId());// directly save
-        // System.out.println("Saved with id: " + customer.getId());
-        // System.out.println("Saved time " + saved.getOrderDateTime()); // confirm id
+        String orderNum = RandomStringUtils.randomAlphanumeric(16);
+        String reciever = customer.getEmailAddress();
+        String subject = "Order + " + orderNum + " + Confirmation";
+        String body = "Thank you for your order";
+        mailSenderService.sendConfirmationEmail(reciever, subject, body);
         return "redirect:/create"; // redirect to avoid template errors
     }
+
+    // @GetMapping("/email")
+    // public void sendConfirmationEmail(){
+    //     String orderNum = RandomStringUtils.randomAlphanumeric(16);
+    //     String reciever = "";
+    //     String subject = "Order +" + orderNum + "+Confirmation";
+    //     String body = "Thank you for your order";
+    //     mailSenderService.sendConfirmationEmail(reciever, subject, body);
+    // }
 
 }
---
+++ src/main/java/com/example/bakerysite/bakerysite/model/MenuItem.java
@@ -0,0 +1,13 @@
+package com.example.bakerysite.bakerysite.model;
+
+public class MenuItem {
+    private String itemName;
+
+    public String getItemName(){
+        return itemName;
+    }
+
+    public void setItemName(String name){
+        this.itemName = name;
+    }
+}
